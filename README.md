bc5e9141f04400caaf4996e75e7756bc06e8bee2
palchurl <palchurl@bxscience.edu>
2021-09-26 11:18:20 -0400

    adding homepage styling and navbar styling

--- README.md
+++ README.md
@@ -8,10 +8,7 @@
 5. This should open up http://localhost/3000/ in your browser 
 
 Then you should be able to see any edits you make to the code files reflected on the app running on http://localhost:3000/ 
-<<<<<<< HEAD
 
-_______________________________________________________________________________________________________
+#### Extra Packages: (npm install) ag-grid-react, ag-grid-community, react-bootstrap, react-router-dom, @material-ui/core
 
-####Extra Packages: (npm install) ag-grid-react, ag-grid-community, react-bootstrap, react-router-dom, @material-ui/core
-=======
-####To download all these packages, just type npm install in terminal
+#### To download all these packages, just type npm install in terminal
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -12,6 +12,51 @@ class Navbar extends Component {
 
   render() {
     return (
+      
+      <nav className="NavbarItems">
+       <div>
+        <Link className="link-item" to="/home">
+        <img className="book-logo"
+           alt="logo of a book"
+           src="/img/icon.png"
+          />
+          <h1 className="navbar-logo">
+            <i className="fas-fa-newspaper"></i>
+          </h1>
+        </Link>
+        </div>
+       
+        <div>
+        <Link className="link-item-right" to="/home">
+          <Button className="bu">Home</Button>&nbsp;
+        </Link>
+        <Link className="link-item-second-right" to="/signup">
+          <Button className="bu">Sign-up</Button>&nbsp;
+        </Link>
+        <Link className="link-item-right" to="/login">
+          <Button className="bu">Log-in</Button>&nbsp;
+        </Link>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
+/*import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+import Button from '@material-ui/core/Button';
+
+class Navbar extends Component {
+  state = { clicked: false };
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+
+  render() {
+    return (
       <nav className="NavbarItems">
         <Link className="link-item" to="/home">
           <h1 className="navbar-logo">
@@ -33,4 +78,4 @@ class Navbar extends Component {
   }
 }
 
-export default Navbar;
+export default Navbar;*/
